662a34f80275a17e5908b7ae2fc8b19a910148e5
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-12-07 15:43:01 -0500

    Fix all errors

--- gravatar.js
+++ gravatar.js
@@ -1,14 +1,11 @@
-var gravatar = function() {
-  function getImage(email, size) {
-    return '<img src="https://secure.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?s=' + size + '&d=mm">';
-  }
+function getImage(email, size) {
+  return '<img src="https://secure.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?s=' + size + '&d=mm">';
+}
 
-  function getImageWithType(email, size, filetype) {
-    return '<img src="https://secure.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?s=' + size + '&d=mm ' + filetype + '">';
-  }
+function getImageWithType(email, size, filetype) {
+  return '<img src="https://secure.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?s=' + size + '&d=mm ' + filetype + '">';
+}
 
-  function getUserProfile(email) {
-    return 'https://secure.gravatar.com/' + md5(email.trim().toLowerCase());
-  }
-
-};
+function getUserProfile(email) {
+  return 'https://secure.gravatar.com/' + md5(email.trim().toLowerCase());
+}
