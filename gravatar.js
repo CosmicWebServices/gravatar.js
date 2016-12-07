f40bda5a119882df33a5a87db1488b5498a55b15
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-12-07 15:11:07 -0500

    Create gravatar.js

---
+++ gravatar.js
@@ -0,0 +1,13 @@
+var gravatar = function() {
+  function getImage(email, size) {
+    return  '<img src="https://secure.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?s=' + size + '&d=mm">';
+  }
+  
+  function getImageWithType(email, size, filetype) {
+    return  '<img src="https://secure.gravatar.com/avatar/' + md5(email.trim().toLowerCase()) + '?s=' + size + '&d=mm ' + filetype'">';
+  }
+
+  function getUserProfile(email) {
+    return  'https://secure.gravatar.com/' + md5(email.trim().toLowerCase())';
+  }
+}
